3d27d82ef5284a4eb57f8c292aebe2f0edf91243
Student <Student@aii.stu>
2018-07-13 14:23:06 -0700

    scaryterry

---
+++ Project1/ScaryTerry.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+
+
+	std::cout << "Hello World Bitch!" << std::endl; 
+
+	std::cout << "So whats 2 numbers multiplied bitch?" << std::endl;
+
+	int num1;
+	std::cin >> num1;
+	int num2; 
+	std::cin >> num2; 
+
+	int answer;
+	answer = num1 * num2;
+
+	std::cout << "the answer is " << answer << " bitch" << std::endl;
+
+
+
+	int input;
+	std::cin >> input;
+
+	return 0;
+}
